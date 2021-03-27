cf9732b811a1d8265c98d836469c0a4bb9b08c62
Ruben <gabrielianruben@gmail.com>
2021-03-28 00:16:12 +0400

    display list items

--- src/components/Card/CartItem.js
+++ src/components/Card/CartItem.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Item = styled.div`
+    background: white;
+    padding: 15px 10px;
+    border-radius: 5px;
+    margin: 10px;
+    cursor: pointer;
+    box-shadow: 0 1px 0 rgb(9 30 66 / 25%);
+    &:hover {
+        background: #f9f9f9;
+    }
+`
 
 function CartItem () {
     return (
-        <div>Cart Itme</div>
+        <Item>Cart Itme</Item>
     )
 }
 
--- src/components/Card/CartList.js
+++ src/components/Card/CartList.js
@@ -2,12 +2,58 @@ import React from 'react';
 import styled from 'styled-components';
 import CartItem from './CartItem';
 
+const List = styled.div`
+    max-width: 300px;
+    background: #ddd;
+    padding: 10px;
+    border-radius: 3px;
+    margin: 25px 0 0 25px;
+    background-color: #ebecf0;
+    .cart-list-top {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding-bottom: 10px;
+        span {
+            font-weight: bold;
+        }
+        span:last-child {
+            font-size: 19px;
+            cursor: pointer;
+        }
+    }
+    .cart-list-bottom {
+        margin: 10px;
+        button {
+            width: 100%;
+            border: none;
+            padding: 10px;
+            font-weight: bold;
+            cursor: pointer;
+            border-radius: 3px;
+            background: transparent;
+            &:hover {
+                background: #f9f9f9;
+            }
+        }
+    }
+`
 
 function CartList () {
     return (
-        <div>
+        <List>
+            <div className='cart-list-top'>
+                <span>Todo</span>
+                <span>...</span>
+            </div>
             <CartItem />
-        </div>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <div className='cart-list-bottom'>
+                <button>+ Add another card</button>
+            </div>
+        </List>
     )
 }
 
